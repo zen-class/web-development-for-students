1a21864363d12f23be8249641af1213d0292e009
sriram radhakrishnan <122258501+sriram-R-krishnan@users.noreply.github.com>
2023-03-27 11:22:08 +0530

    Add files via upload

---
+++ Dice Game - Starter/app.js
@@ -0,0 +1,9 @@
+let gameStatus = document.querySelector('.game-status')
+let reset = document.querySelector(".reset")
+let roll1 = document.querySelector(".player-1-btn")
+let roll2 = document.querySelector(".player-2-btn")
+let score1 = document.querySelector(".score-1")
+let score2 = document.querySelector(".score-2")
+let dice = document.querySelector(".dice-img")
+
+
